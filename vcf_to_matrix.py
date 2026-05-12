ac9e105b972ec60fdd64d908bb5f6a98ee828b3e
Zhan Zhao <zz1461153@gmail.com>
2026-05-11 22:41:29 -0400

    Path to specific file containing genotype data

--- vcf_to_matrix.py
+++ vcf_to_matrix.py
@@ -7,3 +7,14 @@ from sklearn import decomposition # PCA analysis
 import pandas as pd  # Data/Table manipulation
 
 print("Packages loaded successfully")
+
+vcf_filename = "data/ALL.chr22.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz"
+panel_filename = "data/phase1_integrated_calls.20101123.ALL.panel"
+
+import os # For checking file paths and existence, operating system interactions
+
+print(os.getcwd()) # Print the current working directory to verify where the script is being run from
+
+print("VCF exists:", os.path.exists(vcf_filename)) # Check if the VCF file exists at the specified path
+
+print("Panel exists:", os.path.exists(panel_filename)) # Check if the panel file exists at the specified path
